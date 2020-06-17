a6d0dbf226e4c55b3d24033a4ed5373b06d5e491
simongumbleton <simongumbleton@gmail.com>
2020-06-17 23:08:21 +0100

    try to make this work

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ author = 'Simon Gumbleton'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
 ]
 
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -44,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
