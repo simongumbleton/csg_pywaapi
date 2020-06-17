8848ca432c58a30bd113589ae172bb3f066a9a6b
simongumbleton <simongumbleton@gmail.com>
2020-06-17 22:24:28 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..\\csg_pywaapi'))
+sys.path.insert(0, os.path.abspath('../csg_pywaapi'))
 
 
 # -- Project information -----------------------------------------------------
